cf64afcbb8b051c2a66015052b5946fd325e5028
Riccardo Sagramoni <50587373+RiccardoSagramoni@users.noreply.github.com>
2023-07-31 19:38:46 +0200

    nethuns_ioctl_if

--- src/nethuns.rs
+++ src/nethuns.rs
@@ -1,8 +1,9 @@
 use std::ffi::{CStr, CString};
 use std::mem;
-use std::sync::Mutex;
+use std::os::fd::AsRawFd;
 
 use num_traits::AsPrimitive;
+use socket2::{Domain, Socket, Type};
 
 use crate::global::{NethunsNetInfo, NETHUNS_GLOBAL};
 use crate::NethunsSocket;
@@ -14,7 +15,7 @@ pub fn __nethuns_set_if_promisc(
     devname: &CString,
 ) -> Result<(), String> {
     let mut flags =
-        nethuns_ioctl_if(s, devname, SocketConfigurationFlag::SIOCGIFFLAGS, 0)
+        nethuns_ioctl_if(devname, SocketConfigurationFlag::SIOCGIFFLAGS, 0)
             .map_err(|e| {
                 format!(
                     "[__nethuns_set_if_promisc] nethuns_ioctl_if failed: {e}"
@@ -27,10 +28,10 @@ pub fn __nethuns_set_if_promisc(
             None => {
                 mutex_guard
                     .insert(devname.to_owned(), NethunsNetInfo::default());
-                let info = mutex_guard.get_mut(devname).expect(&format!(
+                let info = mutex_guard.get_mut(devname).ok_or(format!(
                     "failed to get info for device {:?}",
                     devname
-                ));
+                ))?;
                 info.promisc_refcnt = if (flags & libc::IFF_PROMISC as u32) == 0
                 {
                     0
@@ -48,7 +49,6 @@ pub fn __nethuns_set_if_promisc(
         if do_promisc {
             flags |= libc::IFF_PROMISC as u32;
             if let Err(e) = nethuns_ioctl_if(
-                s,
                 devname,
                 SocketConfigurationFlag::SIOCSIFFLAGS,
                 flags,
@@ -56,14 +56,13 @@ pub fn __nethuns_set_if_promisc(
                 info.promisc_refcnt -= 1;
                 return Err(format!(
                     "[__nethuns_set_if_promisc] nethuns_ioctl_if failed: {e}"
-                ))
+                ));
             }
         }
         
         if do_promisc {
             eprintln!("device {:?} promisc mode set", devname);
-        }
-        else {
+        } else {
             eprintln!("device {:?} (already) promisc mode set", devname);
         }
     };
@@ -72,17 +71,53 @@ pub fn __nethuns_set_if_promisc(
 }
 
 
+///
 fn nethuns_ioctl_if(
-    s: &impl NethunsSocket,
     devname: &CStr,
     what: SocketConfigurationFlag,
     flags: u32,
 ) -> Result<u32, String> {
-    let x = what as u64;
-    todo!()
+    let socket = Socket::new(Domain::IPV4, Type::DGRAM, None).map_err(|e| {
+        format!("[nethuns_ioctl_if] could not open socket: {e}")
+    })?;
+    
+    let mut ifr: libc::ifreq = unsafe { mem::zeroed() };
+    devname
+        .to_bytes_with_nul()
+        .iter()
+        .take(ifr.ifr_name.len() - 1)
+        .enumerate()
+        .for_each(|(i, c)| {
+            ifr.ifr_name[i] = *c as i8;
+        });
+    
+    if what == SocketConfigurationFlag::SIOCSIFFLAGS {
+        ifr.ifr_ifru.ifru_flags = flags as i16;
+    }
+    
+    let ret = unsafe {
+        libc::ioctl(
+            socket.as_raw_fd(),
+            what as u64,
+            &ifr as *const libc::ifreq as *const libc::c_void,
+        )
+    };
+    if ret < 0 {
+        // FIXME nethuns_perror(nethuns_socket(s)->errbuf, "ioctl");
+        return Err(format!(
+            "[nethuns_ioctl_if] ioctl({:?}, {:?}, {:?}) failed",
+            socket, what, ifr
+        ));
+    }
+    
+    Ok(if what == SocketConfigurationFlag::SIOCGIFFLAGS {
+        unsafe { ifr.ifr_ifru.ifru_flags as u32 }
+    } else {
+        flags
+    })
 }
 
-#[derive(Clone, Copy, Debug)]
+#[derive(Clone, Copy, Debug, PartialEq, PartialOrd)]
 enum SocketConfigurationFlag {
     SIOCGIFFLAGS,
     SIOCSIFFLAGS,
