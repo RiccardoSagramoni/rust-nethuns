ce4aa0e0ac4e60a7b0d95cc451337f9802f7e283
Riccardo Sagramoni <50587373+RiccardoSagramoni@users.noreply.github.com>
2023-10-25 15:42:58 +0200

    Fix string overflow bug
    Fix string overflow bug in `global::setrlimit` which caused the program to leak part of the constants section of the ELF file.

--- src/global.rs
+++ src/global.rs
@@ -27,13 +27,13 @@ pub static NETHUNS_GLOBAL: Mutex<
 ));
 
 
-/// Set RLIMIT_MEMLOCK to infinity at application startup.
+/// Set `RLIMIT_MEMLOCK` to infinity at application startup.
 ///
-/// This function requires CAP_SYS_RESOURCE capability
-/// because the call to [`libc::setrlimit`]
+/// This function requires `CAP_SYS_RESOURCE` capability
+/// because of the call to [`libc::setrlimit`]
 /// (see [setrlimit(2) - Linux man page](https://linux.die.net/man/2/setrlimit)
 /// for more details).
-/// Since this usually means that we must run the tests with root privileges,
+/// Since this would mean that we must run the tests with root privileges,
 /// this function is disabled while testing.
 #[cfg(target_os = "linux")]
 #[cfg(not(test))]
@@ -47,7 +47,7 @@ unsafe fn setrlimit() {
     if ret != 0 {
         libc::fprintf(
             libc::fdopen(libc::STDERR_FILENO, "w+".as_ptr() as _) as _,
-            "nethuns: setrlimit(RLIMIT_MEMLOCK) \"%s\"\n".as_ptr() as _,
+            "nethuns: setrlimit(RLIMIT_MEMLOCK) \"%s\"\n\0".as_ptr() as _,
             libc::strerror(*libc::__errno_location()),
         );
         libc::exit(1);
