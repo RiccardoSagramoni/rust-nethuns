d3c9c9b7910efba82c3440d83050d6bbb7c49520
Riccardo Sagramoni <50587373+RiccardoSagramoni@users.noreply.github.com>
2023-11-10 13:09:39 +0100

    Compile fail pcap

--- tests/compile-fail/pcap_socket_dropped_before_packet.rs
+++ tests/compile-fail/pcap_socket_dropped_before_packet.rs
@@ -1,15 +1,15 @@
+use nethuns::sockets::Local;
 use nethuns::sockets::pcap::NethunsSocketPcap;
 use nethuns::types::NethunsSocketOptions;
 
 fn main() {
-    // TODO
-    // let opt = NethunsSocketOptions::default();
+    let opt = NethunsSocketOptions::default();
     
-    // let socket = NethunsSocketPcap::open(opt, "filename", false).unwrap();
+    let socket = NethunsSocketPcap::<Local>::open(opt, "filename", false).unwrap();
     
-    // let p1 = socket.read().unwrap();
-    // let p2 = socket.read().unwrap();
-    // println!("{:?} {:?}", p1, p2);
-    // drop(socket);
-    // drop(p1);
+    let p1 = socket.read().unwrap();
+    let p2 = socket.read().unwrap();
+    println!("{:?} {:?}", p1, p2);
+    drop(socket);
+    drop(p1);
 }
